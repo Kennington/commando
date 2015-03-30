ff3f4d4466bd65f074b47a4436aeda0c2ca91af9
Kenny Lu <Ken@Defyent.com>
2015-03-30 16:28:39 +0800

    listItem works inefficiently; will add 2x loops

--- commando.js
+++ commando.js
@@ -1,5 +1,18 @@
-if (Meteor.isClient) {
+Posts = new Mongo.Collection ("posts");
 
+if (Meteor.isClient) {
+  Template.listItem.helpers ({
+    'posts': function(){
+      return Posts.find({category:"ideas"});
+    }
+  });
+
+  Template.listItem.events ({
+    'click .listItem' : function (e) {
+      e.preventDefault();
+      console.log("HEY I MADE IT, I'M THE WORLD'S GREATEST");
+    }
+  });
 }
 
 if (Meteor.isServer) {
--- router.js
+++ router.js
@@ -1,17 +1,17 @@
 Router.configure({
-layoutTemplate: 'layout'
+  loadingTemplate: 'loading',
+  notFoundTemplate: 'notFound',
+  // waitOn: function() { return Meteor.subscribe('posts'); }
 });
-
 
 Router.route('/', {
   name: 'home'
   ,
   action: function () {
-    if (Meteor.userId()) {
-      this.render('home');
+    if ( Meteor.user() ) {
+      this.render('mainDisplay');
     } else {
       this.render('loginPage');
     }
   }
 });
-
